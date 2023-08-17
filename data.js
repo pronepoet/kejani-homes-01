c0563d9526f7fcc1be13a815aa688508aa73fa45
Phillip <ochiengphillip84@gmail.com>
2023-08-17 10:00:16 +0300

    Feat: ✨ Added the data to display items

---
+++ data.js
@@ -0,0 +1,38 @@
+const houses = 
+   [ {id: 1,
+    houseDetails: "1 bedroom apartment",
+    location: "Roysambu",
+    price: 9000
+    },
+    {
+        id: 2,
+        houseDetails: "2 bedroom apartment",
+        location: "Ongata Rongai",
+        price: 15000
+    },
+    {
+        id: 3,
+        houseDetails: "4 bedroom apartment",
+        location: "Ruai",
+        price: 90000
+    },
+    {
+        id: 4,
+        houseDetails: "3 bedroom apartment",
+        location: "Ngong Road",
+        price: 55000
+    },
+    {
+        id: 5,
+        houseDetails: "2 bedroom apartment",
+        location: "Kilimani",
+        price: 40000
+    },
+    {
+        id: 6,
+        houseDetails: "1 bedroom apartment",
+        location: "Kawangware",
+        price: 15000
+    }
+   ];
+module.exports = houses;
---
+++ loading.js
@@ -0,0 +1,8 @@
+export default function Loading() {
+    function LoadingSkeleton(){
+        //my loading ui
+        
+    }
+    // You can add any UI inside Loading, including a Skeleton.
+    return <LoadingSkeleton />
+  }
