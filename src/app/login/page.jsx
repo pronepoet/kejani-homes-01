bb2fc578e29b4d18efabb4f0473489c91f60c89b
Phillip <ochiengphillip84@gmail.com>
2023-08-23 04:42:55 +0300

    ✨Feat: Add styling to login route

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -4,9 +4,10 @@ const Login = () => {
   return (
     <>
 
-<div>
+<div className="login_page">
 <form className='login_form' action="">
-            <label className = 'unameLabel' htmlFor="username">Username:</label>
+ 
+  <label className = 'unameLabel' htmlFor="username">Username:</label>
             <br />
             <input className = 'uname' type="text" placeholder="Phone Number/Email Address" id="username" name="username" />
             <br />
@@ -15,10 +16,15 @@ const Login = () => {
             <input className = 'uname' type="password" placeholder="Password"  id="password" name="password" />
             <br />
             <button className= 'loginButton' type="submit">Login</button>
+        
             </form>
             <p className='logo'>&#169; Do not have an account? <Link href="/signup"><span className='signUp'>Sign Up</span></Link> now</p>
             <h3 className='h3'><Link href="/">Back Home</Link></h3>
+
+          
 </div>
+
+
     </>
  
   )
